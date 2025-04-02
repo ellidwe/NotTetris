708374558ddc6ff5a212e9a9d4b76467e7b68f19
BT_2S14_24 <eli.jl.dew@gmail.com>
2025-04-02 11:02:00 -0400

    did the thing

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -2,54 +2,37 @@ import java.awt.*;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
-import java.util.ArrayList;
 
 public class DrawPanel extends JPanel implements MouseListener{
 
-    private boolean[][] grid;
+    private BrickLayout layout = new BrickLayout("src/bricks",40, false);
 
     public DrawPanel()
     {
         this.addMouseListener(this);
-        grid = new boolean[30][40];
-        randomSetGrid();
-    }
-
-    public void randomSetGrid()
-    {
-        long time = System.currentTimeMillis();
-        while(true)
-        {
-            if (System.currentTimeMillis() >= time + 1000)
-            {
-                grid = new boolean[30][40];
-                for(int row = 0; row < 30; row++)
-                {
-                    for(int col = 0; col < 40; col++)
-                    {
-                        if(Math.random() <= 0.3)
-                        {
-                            grid[row][col] = true;
-                        }
-                    }
-                }
-            }
-        }
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        for(int i = 0; i < 40; i++)
+        long time = System.currentTimeMillis();
+
+        while(!(System.currentTimeMillis() >= time + 500))
         {
-            for(int j = 0; j < 30; j++)
+
+        }
+        time = System.currentTimeMillis();
+        layout.doOneBrick();
+        for(int row = 0; row < 40; row++)
+        {
+            for(int col = 0; col < 30; col++)
             {
-                g.drawRect(10 + (25 * i), 10 + (25 * j), 20, 20);
+                g.drawRect(10 + (25 * row), 10 + (25 * col), 20, 20);
                 Graphics2D g2 = (Graphics2D) g;
-                if(grid[j][i])
+                if(layout.checkBrickSpot(col, row))
                 {
                     g2.setColor(Color.RED);
-                    g2.fillRect(10 + (25 * i), 10 + (25 * j), 20, 20);
+                    g2.fillRect(10 + (25 * row), 10 + (25 * col), 20, 20);
                     g2.setColor(Color.BLACK);
                 }
             }
@@ -59,7 +42,7 @@ public class DrawPanel extends JPanel implements MouseListener{
     @Override
     public void mouseClicked(MouseEvent e)
     {
-        randomSetGrid();
+
     }
 
     @Override
